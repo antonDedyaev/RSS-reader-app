9dca1c59fbf7aeb6ee902a12b3cde27bfa22fd43
Aintdead86 <96647913+Aintdead86@users.noreply.github.com>
2022-09-05 11:48:08 +0300

    use single object with elements in renderers

--- src/view.js
+++ src/view.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import onChange from 'on-change';
 
 const modalHandler = (e, post) => {
@@ -5,8 +6,8 @@ const modalHandler = (e, post) => {
   const modalHeader = document.querySelector('.modal-title');
   const modalBody = document.querySelector('.modal-body');
   const modalArticle = document.querySelector('.full-article');
-  modalHeader.innerHTML = post.title;
-  modalBody.innerHTML = post.description;
+  modalHeader.textContent = post.title;
+  modalBody.textContent = post.description;
   modalArticle.href = post.link;
 };
 
@@ -17,7 +18,7 @@ const buildContainerBody = (i18nextInstance, i18text, listElements) => {
   mainDivBody.classList.add('card-body');
   const mainDivHeader = document.createElement('h2');
   mainDivHeader.classList.add('card-title', 'h4');
-  mainDivHeader.innerHTML = i18nextInstance.t(i18text);
+  mainDivHeader.textContent = i18nextInstance.t(i18text);
   mainDivBody.append(mainDivHeader);
   const ulEl = document.createElement('ul');
   ulEl.classList.add('list-group', 'border-0', 'rounded-0');
@@ -26,80 +27,72 @@ const buildContainerBody = (i18nextInstance, i18text, listElements) => {
   return mainDiv;
 };
 
-const renderProcessState = (status, i18nextInstance) => {
-  const rssForm = document.querySelector('.rss-form');
-  const input = document.getElementById('url-input');
-  const submitButton = document.querySelector('.px-sm-5');
-  const feedBack = document.querySelector('.feedback');
-
+const renderProcessState = (elements, status, i18nextInstance) => {
   switch (status) {
     case 'filling':
-      submitButton.disabled = false;
-      input.classList.remove('is-invalid');
-      rssForm.reset();
+      elements.submitButton.disabled = false;
+      elements.input.classList.remove('is-invalid');
+      elements.rssForm.reset();
       break;
     case 'loading':
-      submitButton.disabled = true;
+      elements.submitButton.disabled = true;
       break;
     case 'success':
-      submitButton.disabled = false;
-      input.classList.remove('is-invalid');
-      feedBack.classList.remove('text-danger');
-      feedBack.classList.add('text-success');
-      feedBack.innerHTML = i18nextInstance.t('uIElements.successMessage');
-      rssForm.reset();
+      elements.submitButton.disabled = false;
+      elements.input.classList.remove('is-invalid');
+      elements.feedBack.classList.remove('text-danger');
+      elements.feedBack.classList.add('text-success');
+      elements.feedBack.textContent = i18nextInstance.t('uiElements.successMessage');
+      elements.rssForm.reset();
       break;
     case 'fault':
-      submitButton.disabled = false;
-      input.classList.add('is-invalid');
-      feedBack.classList.remove('text-success');
-      feedBack.classList.add('text-danger');
+      elements.submitButton.disabled = false;
+      elements.input.classList.add('is-invalid');
+      elements.feedBack.classList.remove('text-success');
+      elements.feedBack.classList.add('text-danger');
       break;
     default:
       throw new Error(`Unknown process state: ${status}`);
   }
-  input.focus();
+  elements.input.focus();
 };
 
-const renderErrors = (errors, i18nextInstance) => {
-  const errorField = document.querySelector('.feedback');
-  errorField.innerHTML = errors ? i18nextInstance.t(`errors.${errors}`) : null;
+const renderErrors = (elements, errors, i18nextInstance) => {
+  elements.feedBack.textContent = errors ? i18nextInstance.t(`errors.${errors}`) : null;
 };
 
-const renderFeeds = (feeds, i18nextInstance) => {
-  const container = document.querySelector('.feeds');
-  container.innerHTML = '';
+const renderFeeds = (elements, feeds, i18nextInstance) => {
+  elements.feedsContainer.textContent = '';
 
   const feedList = feeds.map((feed) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item', 'border-0', 'border-end-0');
     const liTitle = document.createElement('h3');
     liTitle.classList.add('h6', 'm-0');
-    liTitle.innerHTML = feed.title;
+    liTitle.textContent = feed.title;
     const liDescription = document.createElement('p');
     liDescription.classList.add('m-0', 'small', 'text-black-50');
-    liDescription.innerHTML = feed.description;
+    liDescription.textContent = feed.description;
     liEl.append(liTitle, liDescription);
     return liEl;
   });
-  const containerBody = buildContainerBody(i18nextInstance, 'uIElements.feedTitle', feedList);
-  container.append(containerBody);
+  const containerBody = buildContainerBody(i18nextInstance, 'uiElements.feedTitle', feedList);
+  elements.feedsContainer.append(containerBody);
 };
 
-const renderPosts = (state, i18nextInstance) => {
-  const container = document.querySelector('.posts');
-  container.innerHTML = '';
+const renderPosts = (elements, state, i18nextInstance) => {
+  elements.postsContainer.textContent = '';
 
   const postList = state.posts.map((post) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
     const aEl = document.createElement('a');
     aEl.setAttribute('href', post.link);
-    aEl.classList.add(state.viewedPosts.includes(post.id) ? ('fw-normal', 'link-secondary') : ('fw-bold'));
+    aEl.classList.add(state.uiState.viewedPosts.includes(post.id) ? ('fw-normal', 'link-secondary') : ('fw-bold'));
     aEl.setAttribute('data-id', post.id);
     aEl.setAttribute('target', '_blank');
     aEl.setAttribute('rel', 'noopener noreferrer');
-    aEl.innerHTML = post.title;
+    aEl.textContent = post.title;
 
     const viewButton = document.createElement('button');
     viewButton.classList.add('btn', 'btn-outline-primary', 'btn-sm');
@@ -107,13 +100,13 @@ const renderPosts = (state, i18nextInstance) => {
     viewButton.setAttribute('data-id', post.id);
     viewButton.setAttribute('data-bs-toggle', 'modal');
     viewButton.setAttribute('data-bs-target', '#modal');
-    viewButton.innerHTML = i18nextInstance.t('uIElements.viewButton');
+    viewButton.textContent = i18nextInstance.t('uiElements.viewButton');
     viewButton.addEventListener('click', (e) => modalHandler(e, post));
     liEl.append(aEl, viewButton);
     return liEl;
   });
-  const containerBody = buildContainerBody(i18nextInstance, 'uIElements.postTitle', postList);
-  container.append(containerBody);
+  const containerBody = buildContainerBody(i18nextInstance, 'uiElements.postTitle', postList);
+  elements.postsContainer.append(containerBody);
 };
 
 const renderViewedPosts = (viewedPosts) => {
@@ -124,22 +117,22 @@ const renderViewedPosts = (viewedPosts) => {
   });
 };
 
-const watcher = (state, i18nextInstance) => onChange(state, (path) => {
+const watcher = (elements, state, i18nextInstance) => onChange(state, (path) => {
   switch (path) {
-    case 'rssForm.processState':
-      renderProcessState(state.rssForm.processState, i18nextInstance);
+    case 'processState':
+      renderProcessState(elements, state.processState, i18nextInstance);
       break;
     case 'rssForm.errors':
-      renderErrors(state.rssForm.errors, i18nextInstance);
+      renderErrors(elements, state.rssForm.errors, i18nextInstance);
       break;
     case 'feeds':
-      renderFeeds(state.feeds, i18nextInstance);
+      renderFeeds(elements, state.feeds, i18nextInstance);
       break;
     case 'posts':
-      renderPosts(state, i18nextInstance);
+      renderPosts(elements, state, i18nextInstance);
       break;
-    case 'viewedPosts':
-      renderViewedPosts(state.viewedPosts);
+    case 'uiState.viewedPosts':
+      renderViewedPosts(state.uiState.viewedPosts);
       break;
     default:
       throw new Error(`Unknown path: ${path}`);
